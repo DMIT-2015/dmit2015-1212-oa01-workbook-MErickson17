dd5333698db5c95f2123607021c1d52a0f1ce12c
Mackenzie Erickson <mdqe@hotmail.com>
2022-03-14 12:27:33 -0600

    end of class March 14/22

---
+++ dmit2015-1212-timer-demo/src/main/java/common/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package common.config;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.annotation.sql.DataSourceDefinitions;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@DataSourceDefinitions({
+
+	@DataSourceDefinition(
+		name="java:app/datasources/mssqlDS",
+		className="com.microsoft.sqlserver.jdbc.SQLServerDataSource",
+		url="jdbc:sqlserver://DMIT-Capstone1.ad.sast.ca;databaseName=DMIT2015_1212_A01_merickson17;TrustServerCertificate=true",   // change A01 to E01 if you are in section E01, change yourNaitUsername
+		user="merickson17",  //  change yourNaitUsername
+		password="RemotePassword200488405"),    // Replace 200012345 with your NAIT StudentID
+
+})
+
+@ApplicationScoped
+public class ApplicationConfig {
+
+}
