19d68f77477d230c2e5cffd43b2e520df8fbeaa1
Mackenzie Erickson <mdqe@hotmail.com>
2022-03-23 11:31:25 -0600

    end of class March 23

--- dmit2015-1212-jsf-demo/src/main/java/dmit2015/repository/MovieRepository.java
+++ dmit2015-1212-jsf-demo/src/main/java/dmit2015/repository/MovieRepository.java
@@ -5,12 +5,25 @@ import dmit2015.entity.Movie;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+
 @ApplicationScoped
 @Transactional
 public class MovieRepository extends AbstractJpaRepository<Movie, Long> {
 
     public MovieRepository() {
         super(Movie.class);
+    }
+
+    public List<Movie> listByReleaseDateDescending() {
+        return getEntityManager()
+                .createQuery("""
+                    SELECT m
+                    from Movie m
+                    ORDER BY m.releaseDate DESC                    
+                """,
+                        Movie.class)
+                .getResultList();
     }
 
 }
