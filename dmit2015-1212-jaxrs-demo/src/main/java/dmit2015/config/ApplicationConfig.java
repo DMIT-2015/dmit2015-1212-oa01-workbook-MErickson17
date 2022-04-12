1d1c7606c6fb06fb5d9aade8b9fb446c1b488e24
Mackenzie Erickson <mdqe@hotmail.com>
2022-04-12 09:51:50 -0600

    End of Class 12-04-2022

--- dmit2015-1212-jaxrs-demo/src/main/java/dmit2015/config/ApplicationConfig.java
+++ dmit2015-1212-jaxrs-demo/src/main/java/dmit2015/config/ApplicationConfig.java
@@ -3,22 +3,22 @@ import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.annotation.sql.DataSourceDefinitions;
 import jakarta.enterprise.context.ApplicationScoped;
 @DataSourceDefinitions({
+//
+//		@DataSourceDefinition(
+//                name="java:app/datasources/h2databaseDS",
+//                className="org.h2.jdbcx.JdbcDataSource",
+////                url="jdbc:h2:file:~/jdk/databases/dmit2015-jaxrs-db", // if you want to save file to system between the application running
+//            	url="jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", // will not persist data
+//                user="sa",
+//                password="sa"),
 
-		@DataSourceDefinition(
-                name="java:app/datasources/h2databaseDS",
-                className="org.h2.jdbcx.JdbcDataSource",
-//                url="jdbc:h2:file:~/jdk/databases/dmit2015-jaxrs-db", // if you want to save file to system between the application running
-            	url="jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", // will not persist data
-                user="sa",
-                password="sa"),
-
-//        @DataSourceDefinition(
-//                name="java:app/datasources/hsqldatabaseDS",
-//                className="org.hsqldb.jdbc.JDBCDataSource",
-//      url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-jaxrs-demos-hsqldb;shutdown=true",
-////                url="jdbc:hsqldb:mem:dmit2015hsqldb",
-//                user="user2015",
-//                password="Password2015"),
+        @DataSourceDefinition(
+                name="java:app/datasources/hsqldatabaseDS",
+                className="org.hsqldb.jdbc.JDBCDataSource",
+      url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-jaxrs-demos-hsqldb;shutdown=true",
+//                url="jdbc:hsqldb:mem:dmit2015hsqldb",
+                user="user2015",
+                password="Password2015"),
 
 //      @DataSourceDefinition(
 //          name="java:app/datasources/oracleUser2015DS",
